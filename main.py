cd9efb4bd6fa13f2806adf1193a333076832e44a
Belal123-hub <131976726+Belal123-hub@users.noreply.github.com>
2024-06-17 15:25:44 +0700

    check for constraints like 1 <= m, n <= 100

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        row = [1] * n
+        if not (1 <= m <= 100 and 1 <= n <= 100):
+            raise ValueError("m and n must be between 1 and 100 inclusive.")
 
+        row = [1] * n
         for i in range(m - 1):
             newRow = [1] * n
             for j in range(n - 2, -1, -1):
